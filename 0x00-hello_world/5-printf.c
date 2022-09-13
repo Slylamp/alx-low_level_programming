bf41c52a5f9dced7f6d5cb4a39694295923df83b
Slylamp <swendomba@gmail.com>
2022-09-13 15:00:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  print string from the put function
+ *
+ * Description: using the main function
+ * this progrma prints "Programming is like building a multiligual puzzel
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but 4the outcome is a piece of art,\n");
+	return (0);
+}
