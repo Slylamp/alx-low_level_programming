7a9fd2741c1a769a25d86d2be8f9e4b2339931b6
Slylamp <swendomba@gmail.com>
2022-09-21 23:51:12 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,17 @@
-#include "main.h"
+#include <unistd.h>
+
+/**
+* _putchar - writes the character c stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and error is there is returned
+*/
+int _putchar(char c)
+{
+        return write(1, &c, 1);
+}
+
 
 /**
  * _puts - write a function that prints a string, followed by a new line, to
