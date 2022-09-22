279ee7bc54b0206285d75f8b001be94d976ae6e3
Slylamp <swendomba@gmail.com>
2022-09-21 18:00:16 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,14 +12,14 @@ void print_rev(char *s)
 int i;
 
 i = 0;
-while (*s(i) != '\0')
+while (s(i) != '\0');
 {
 i++;
 }
 
-for (i = i - i ; i >= 0; i--)
+for (i = i - i ; i >= 0; i--);
 {
-_putchar (*s[i]);
+_putchar (s[i]);
 }
 
 _putchar ('\n');
