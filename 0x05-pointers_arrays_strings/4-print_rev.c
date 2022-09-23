453d90175522fab7374718a433e7cbdc77a5ca67
Slylamp <swendomba@gmail.com>
2022-09-22 17:03:35 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,4 @@
-#include <unistd.h>
-
-/**
-* _putchar - writes the character c stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and error is there is returned
-*/
-
-int _putchar(char c)
-{
-        return write(1, &c, 1);
-}
-
+#include "main.h"
 
 /**
  * print_rev - This funtion prints a reverse string
